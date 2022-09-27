f3aea239df002a2f773d453dd4eec166cb211ee3
jiwon <sjgirls88@naver.com>
2022-09-27 12:03:02 +0900

    0927 수정2

--- src/main/resources/static/js/post/review/addForm.js
+++ src/main/resources/static/js/post/review/addForm.js
@@ -1,7 +1,7 @@
 'use strict';
 //textArea => ck_editor 대체
 ClassicEditor
-		.create( document.querySelector( '#bcontent' ), {
+		.create( document.querySelector( '#reviewPostContent' ), {
 		 plugin:['ListStyle','Markdown','MediaEmbed','MediaEmbedToolbar'],
 			toolbar: {
 				items: [
@@ -63,20 +63,20 @@ ClassicEditor
 			console.error( error );
 		} );
 
-const $bbs = document.querySelector('section');
-const category = ($bbs?.dataset.code)? $bbs.dataset.code : '';
-
+//const $bbs = document.querySelector('.bbs-wrap');
+//const category = ($bbs?.dataset.code)? $bbs.dataset.code : '';
+//
 ////등록
 //const $writeBtn = document.getElementById('writeBtn');
 //$writeBtn?.addEventListener("click", e=>{
 //  writeForm.submit();
 //});
-//목록
+////목록
 //const $listBtn = document.getElementById('listBtn');
 //$listBtn?.addEventListener("click",e=>{
 //  const url = `/bbs/list?category=${category}`;
 //  location.href = url;
 //});
-//분류자동 선택
+////분류자동 선택
 //const $options = document.querySelectorAll('#bcategory option');
 //[...$options].find(option=>option.value===category).setAttribute('selected','selected');
